0df22aebf3e65c5693fd29f91346aa3359bbc66c
MaiXiaochai <maixiaochai@qq.com>
2020-10-30 16:54:11 +0800

    +)oracle_utils.py

---
+++ oracle_utils.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+--------------------------------------
+@File       : oracle_utils.py
+@Author     : maixiaochai
+@Email      : maixiaochai@outlook.com
+@CreatedOn  : 2020/10/30 16:48
+--------------------------------------
+"""
+from cx_Oracle import makedsn, connect
+
+
+class OracleUtils:
+    def __init__(self,
+                 username: str,
+                 password: str,
+                 host: str,
+                 port: str,
+                 service_name: str = None,
+                 sid: str = None):
+
+        dns = ''
+        if service_name:
+            dns = makedsn(host, port, service_name=service_name)
+
+        elif sid:
+            dns = makedsn(host, port, sid=sid)
+
+        self.conn = connect(username, password, dns)
+        self.cur = self.conn.cursor()
+
+    def execute(self, sql, args=None):
+        if args:
+            self.cur.execute(sql, args)
+        else:
+            self.cur.execute(sql)
+
+    def executemany(self, sql, args):
+        self.cur.executemany(sql, args)
+        self.commit()
+
+    def fetchall(self, sql, args=None):
+        self.execute(sql, args)
+        result = self.cur.fetchall()
+
+        return result
+
+    def fetchone(self, sql, args=None):
+        self.execute(sql, args)
+        result = self.cur.fetchone()
+
+        return result
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+
+    def close(self):
+        try:
+            self.cur.close()
+            self.conn.close()
+        except Exception as err:
+            print(str(err))
+
+    def __del__(self):
+        self.close()
